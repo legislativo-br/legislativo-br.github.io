b526f060ff1ec50d29e330ed3d87528149ca502e
Emanuel Souza <emanuelgdesouza@gmail.com>
2019-09-04 16:58:33 -0300

    update menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,25 +1,106 @@
 import React from 'react';
-import { withRouter } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
 import styled from 'styled-components';
-import LinkButton from './Button';
+import Logo from '../assets/images/logo.png';
 
-const onHome = location => location.pathname === '/';
-
-const Container = styled.p`
-  position: absolute;
-  top: 5px;
-  right: 5px;
+const Container = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  position: fixed;
+  top: 0;
+  right: 0;
   z-index: 2;
+  width: 100%;
+  padding: 1rem 2rem;
+  background-color: #34314D;
+  box-shadow: 0 10px 15px -3px rgba(0,0,0,.1), 0 4px 6px -2px rgba(0,0,0,.05);
+
+  @media screen and (max-width: 552px) {
+    padding: 1rem;
+  }
+`;
+
+const LogoContainer = styled.figure`
+  width: 50px;
+  height: 50px;
+  overflow: hidden;
+  border-radius: 100%;
+`;
+
+const LinksItem = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  font-size: 1.3rem;
+
+  .link-item {
+    position: relative;
+    color: white;
+    padding: 0 1.25rem;
+
+    &::before {
+      content: '';
+      display: block;
+      position: absolute;
+      top: -5px;
+      right: 0;
+      width: 100%;
+      height: 2px;
+      background-color: #F6B642;
+      opacity: 0;
+      transition: opacity 0.3s ease-out;
+    }
+
+    &:hover {
+      &::before {
+        opacity: 1;
+      }
+    }
+
+    @media screen and (max-width: 552px) {
+      margin: 0 0.25rem;
+      padding: 0 0.25rem;
+    }
+  }
+
+  .external-link {
+    color: white;
+    font-size: 1.5rem;
+    padding-left: 1.25rem;
+
+    &:hover {
+      color: #F6B642;
+    }
+
+    @media screen and (max-width: 552px) {
+      padding-left: 0.25rem;
+    }
+  }
 `;
 
 const Menu = props => {
-  if (onHome(props.location || {})) {
-    return null
-  }
-
   return (
     <Container>
-      <LinkButton label="Página inicial" route="/" />
+      <Link to="/">
+        <LogoContainer>
+          <img src={Logo} alt="" />
+        </LogoContainer>
+      </Link>
+      
+      <LinksItem>
+        <Link className="link-item" to="/states"> Estados </Link>
+
+        <Link className="link-item" to="/about"> Sobre </Link>
+
+        <a
+          target="blank"
+          className="external-link"
+          href="https://github.com/legislative-votes-br"
+          title="Visite o repositório do projeto"
+        >
+          <i className="fab fa-github-square"></i>
+        </a>
+      </LinksItem>
     </Container>
   )
 };
