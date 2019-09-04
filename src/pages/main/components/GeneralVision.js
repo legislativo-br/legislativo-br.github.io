8d0167c215548d4fffbc181e1a626036dac4d8fc
Emanuel Souza <emanuelgdesouza@gmail.com>
2019-09-04 15:46:11 -0300

    GeneralVision: replace Chart and use Pictogram component

--- src/pages/main/components/GeneralVision.js
+++ src/pages/main/components/GeneralVision.js
@@ -3,49 +3,55 @@ import styled from 'styled-components';
 
 import generalSummaryData from '../../../data/general-summary.json';
 import { Section, Container, Content } from '../../../styles';
-import BarChart from '../../../components/BarChart';
+import Pictogram from '../../../components/Pictogram';
 import LinkButton from '../../../components/Button';
 
+const { not_pulling_qty, chars } = generalSummaryData[0] || {}
+
 const GeneralVisionContainer = styled(Section)`
-  background-color: white;
+  background-color: #34314D;
+  color: white;
+
+  .info {
+    max-width: 700px;
+    margin: 0 auto;
+    text-align: center;
+  }
+
+  .pictogram-container {
+    max-width: 500px;
+    margin: 2rem auto;
+  }
 `;
 
 export default class GeneralVision extends Component {
-  constructor(props) {
-    super(props)
-  
-    this.state = {
-      series: this.getSeries()
-    }
-  }
-
-  getSeries () {
-    const data = generalSummaryData[0]
-    const { not_pulling_qty, chars } = data
-
-    return [{
-      name: 'Eleitos Nominalmente',
-      data: [ not_pulling_qty ]
-    }, {
-      name: 'Eleitos por puxadinho',
-      data: [ (chars - not_pulling_qty) ]
-    }]
-  }
-  
   render() {
-    const { series } = this.state
+    const proportion = Math.floor((not_pulling_qty / chars) * 100)
+
     return (
       <GeneralVisionContainer>
         <Content center>
           <Container>
-            <h2> Visão Geral </h2>
-            <p> A seguir, um gráfico que mostra o quantidade de cadeiras ocupadas por deputados eleitos nominalmente ou por <a href="http://www.tse.jus.br/eleitor/glossario/termos/quociente-partidario" title="Acesse a explicação do TSE a respeito do termo Quociente Partidário">Quociente Partidário</a> </p>
-            <BarChart series={series} />
-            <br />
-            <LinkButton
-              label="Veja os dados por Estado"
-              route="/states"
-            />
+            <div className="info">
+              <h2> Visão Geral </h2>
+              <p> A seguir, um gráfico que mostra a proporção de cadeiras ocupadas por deputados eleitos nominalmente ou por <a className="link is-inverted" href="http://www.tse.jus.br/eleitor-e-eleicoes/eleitor/glossario/termos/quociente-eleitoral" title="Acesse a explicação do TSE a respeito do termo Quociente Eleitoral">Quociente Eleitoral</a>. Sendo assim, dos <span className="mark-white">513</span> lugares disputados, apenas <span className="mark-white">26</span> foram ocupados por candidatos que conseguiram se eleger obtendo mais votos que o Quociente Eleitoral. Isso representa apenas <span className="mark-white">5%</span> das cadeiras no Congresso Nacional. </p>
+            </div>
+
+            <div className="pictogram-container">
+              <Pictogram
+                chars={100}
+                inverted
+                selected={proportion}
+              />
+            </div>
+
+            <div>
+              <LinkButton
+                type="is-secondary"
+                label="Veja os dados por Estado"
+                route="/states"
+              />
+            </div>
           </Container>
         </Content>
       </GeneralVisionContainer>
--- src/pages/main/components/Header.js
+++ src/pages/main/components/Header.js
@@ -7,14 +7,17 @@ const Header = props => {
     <Hero>
       <Content center>
         <Container>
-          <p> O projeto "Como meu deputado foi eleito?", tem como objetivo trazer para o contexto dos eleitores, dados das Eleições Federais de 2018, a fim de informar a respeito da quantidade de deputados que foram eleitos diretamente pelos seus votos (eleitos nominalmente), e comparar o quantitativo em relação aos que foram "puxados" (eleitos por <a href="http://www.tse.jus.br/eleitor/glossario/termos/quociente-partidario" title="Acesse a explicação do TSE a respeito do termo Quociente Partidário">Quociente Partidário</a>). </p>
+          <p> O projeto "Como meu deputado foi eleito?", tem como objetivo trazer para o contexto dos eleitores, dados das Eleições Federais de 2018, a fim de informar a respeito da quantidade de deputados que foram eleitos diretamente pelos seus votos (eleitos nominalmente), e comparar o quantitativo em relação aos que foram "puxados" (eleitos por <a className="link" href="http://www.tse.jus.br/eleitor/glossario/termos/quociente-partidario" title="Acesse a explicação do TSE a respeito do termo Quociente Partidário">Quociente Partidário</a>). </p>
 
-          <p> Nesta página você poderá analisar os dados nacionalmente, como na próxima seção, bem como visualizar os candidatos que foram eleitos nominalmente (conseguiram uma quantidade de votos acima do <a href="http://www.tse.jus.br/eleitor-e-eleicoes/eleitor/glossario/termos/quociente-eleitoral" title="Acesse a explicação do TSE a respeito do termo Quociente Eleitoral">Quociente Eleitoral</a>) </p>
+          <p> Nesta página você poderá analisar os dados nacionalmente, como na próxima seção, bem como visualizar os candidatos que foram eleitos nominalmente (conseguiram uma quantidade de votos acima do <a className="link" href="http://www.tse.jus.br/eleitor-e-eleicoes/eleitor/glossario/termos/quociente-eleitoral" title="Acesse a explicação do TSE a respeito do termo Quociente Eleitoral">Quociente Eleitoral</a>) </p>
 
           <br />
-          <PrimaryButton onClick={() => props.history.push("/about")}>
-            Conheça mais do projeto
-          </PrimaryButton>
+
+          <div>
+            <PrimaryButton onClick={() => props.history.push("/about")}>
+              Conheça mais do projeto
+            </PrimaryButton>
+          </div>
         </Container>
       </Content>
     </Hero>
