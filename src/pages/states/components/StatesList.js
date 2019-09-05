c98b374698ec95a1f19fb065b19c6c8bf970564f
Emanuel Souza <emanuelgdesouza@gmail.com>
2019-09-05 11:04:49 -0300

    eslint fix

--- src/pages/states/components/StatesList.js
+++ src/pages/states/components/StatesList.js
@@ -1,5 +1,4 @@
-import React, { useState } from 'react';
-import styled from 'styled-components';
+import React from 'react';
 import data from '../../../data/states-summary.json';
 import StateCard from './StateCard';
 
