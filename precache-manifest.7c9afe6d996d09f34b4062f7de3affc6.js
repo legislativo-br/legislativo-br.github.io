3ade4d9b450693926650c4c58090c4d5466fbf64
Emanuel Souza <emanuelgdesouza@gmail.com>
2019-09-09 20:14:59 -0300

    a1a7511

---
+++ precache-manifest.7c9afe6d996d09f34b4062f7de3affc6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be0195183ee206168f028502b6ea3dc8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f3ad99ce8bfa39d13b62",
+    "url": "/static/css/main.7822498f.chunk.css"
+  },
+  {
+    "revision": "39f72204e5b26035eb23",
+    "url": "/static/js/2.761cb4e7.chunk.js"
+  },
+  {
+    "revision": "f3ad99ce8bfa39d13b62",
+    "url": "/static/js/main.8351e986.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "482f2b88c21bbadfcd484ecfa0a46e66",
+    "url": "/static/media/camara.482f2b88.jpg"
+  },
+  {
+    "revision": "757f40c45d7a73c367eb42d7c24a77ef",
+    "url": "/static/media/desktop-banner.757f40c4.jpg"
+  },
+  {
+    "revision": "74f6881a0d5333732fb1adc14425552f",
+    "url": "/static/media/logo.74f6881a.png"
+  },
+  {
+    "revision": "479874b4d6d8b26825088e2fd2983e12",
+    "url": "/static/media/mobile-banner.479874b4.jpg"
+  }
+]);
